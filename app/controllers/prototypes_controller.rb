29eaec2e9f520e04adfe8d7a864f4991245ee6bd
toshikatsu1201 <hitofuta1201@icloud.com>
2024-10-18 15:49:59 +0900

    編集削除機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,54 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :update, :show, :destroy]
+  
   def index
-    # @prototypes = Prototypes.all
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+  
+    if @prototype.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+   
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+      #成功したら詳細ページに行く
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:image, :concept, :catch_copy, :title).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,11 @@
 class Prototype < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_one_attached :image
+
+  validates :catch_copy, presence: true
+  validates :title, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+
 end
